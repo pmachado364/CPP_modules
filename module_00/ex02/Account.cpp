89af9ed02bd09c127d7fad11b889a169e404826b
pmachado <silva.mmmpedro@gmail.com>
2025-08-23 15:40:06 +0100

    To do ex02

---
+++ module_00/ex02/Account.cpp
@@ -0,0 +1,27 @@
+void	Account::displayTimestamp(void)
+{
+}
+
+void Account::displayAccountsInfos()
+{
+}
+
+Account::Account(int initial_deposit)
+{
+}
+
+void	Account::displayStatus( void ) const
+{
+}
+
+void Account::makeDeposit( int deposit )
+{
+}
+
+bool Account::makeWithdrawal( int withdrawal )
+{
+}
+
+Account::~Account()
+{
+}
