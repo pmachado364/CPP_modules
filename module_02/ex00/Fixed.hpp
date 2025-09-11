b03047032ff0d7a4fe9646e370557c454c015ddf
pmachado <silva.mmmpedro@gmail.com>
2025-09-11 17:06:59 +0100

    [wip] Add Fixed class definition with private member variables

---
+++ module_02/ex00/Fixed.hpp
@@ -0,0 +1,14 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int fpvalue; //int para armazenar o fixed point
+		static const int fractionalBits = 8; //numero de fractional bits
+	public:
+		
+}
+
+#endif
